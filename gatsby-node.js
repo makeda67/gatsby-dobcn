160f125295414e5bf6f58019d9b0097dd9090eea
Aleix Maqueda <aleixmaqueda97@gmail.com>
2019-10-16 16:33:47 +0200

    Uncomented plugins and graphql calls that make errors

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,45 +1,45 @@
-// gatsby-node.js
+// // gatsby-node.js
 
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+// /**
+//  * Implement Gatsby's Node APIs in this file.
+//  *
+//  * See: https://www.gatsbyjs.org/docs/node-apis/
+//  */
 
-// You can delete this file if you're not using it
-const path = require(`path`)
-const { createFilePath } = require(`gatsby-source-filesystem`)
+// // You can delete this file if you're not using it
+// const path = require(`path`)
+// const { createFilePath } = require(`gatsby-source-filesystem`)
 
-exports.createPages = async ({ graphql, actions, reporter }) => {
-  const { createPage } = actions
-  const BlogPostTemplate = path.resolve("./src/templates/BlogPost.js")
+// exports.createPages = async ({ graphql, actions, reporter }) => {
+//   const { createPage } = actions
+//   const BlogPostTemplate = path.resolve("./src/templates/BlogPost.js")
 
-  const result = await graphql(`
-    {
-      allWordpressPost {
-        edges {
-          node {
-            slug
-            wordpress_id
-          }
-        }
-      }
-    }
-  `)
+//   // const result = await graphql(`
+//   //   {
+//   //     allWordpressPost {
+//   //       edges {
+//   //         node {
+//   //           slug
+//   //           wordpress_id
+//   //         }
+//   //       }
+//   //     }
+//   //   }
+//   // `)
 
-  if (result.errors) {
-    reporter.panicOnBuild(`Error while running GraphQL query.`)
-    return
-  }
+//   if (result.errors) {
+//     reporter.panicOnBuild(`Error while running GraphQL query.`)
+//     return
+//   }
 
-  const BlogPosts = result.data.allWordpressPost.edges
-  BlogPosts.forEach(post => {
-    createPage({
-      path: `/post/${post.node.slug}`,
-      component: BlogPostTemplate,
-      context: {
-        id: post.node.wordpress_id,
-      },
-    })
-  })
-}
+//   const BlogPosts = result.data.allWordpressPost.edges
+//   BlogPosts.forEach(post => {
+//     createPage({
+//       path: `/post/${post.node.slug}`,
+//       component: BlogPostTemplate,
+//       context: {
+//         id: post.node.wordpress_id,
+//       },
+//     })
+//   })
+// }
