d675cf5c7048f93f0f75ff4b8151060c2a388155
Aleix Maqueda <aleixmaqueda97@gmail.com>
2019-11-02 14:41:46 +0100

    Updates

---
+++ component---src-pages-index-js-eede89934bc5fefcfa0a.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{209:function(n,o,t){"use strict";t.r(o),t.d(o,"default",(function(){return i}));t(0);function i(){return"undefined"!=typeof window&&(window.location="/catalog"),null}}}]);
+//# sourceMappingURL=component---src-pages-index-js-eede89934bc5fefcfa0a.js.map
---
+++ styles-60b64de2c846cbe5cffe.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{211:function(n,w,o){}}]);
+//# sourceMappingURL=styles-60b64de2c846cbe5cffe.js.map
